8311828f801dbfb8d9dc3fbb67935b1bd7b5b34a
kbentley1 <46969522+kbentley1@users.noreply.github.com>
2021-10-21 16:08:01 +0100

    Add BinaryTree Sum Left or Right Bigger

---
+++ UnitTests/TestLeftOrRightOfBinaryTreeSumLarger.cs
@@ -0,0 +1,29 @@
+﻿using Shouldly;
+using TestProject;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TestLeftOrRightOfBinaryTreeSumLarger
+    {
+        [Theory]
+        [InlineData(new long[]{}, "")]
+        [InlineData(new long[]{1}, "")]
+        [InlineData(new long[]{1,1,1}, "")]
+        [InlineData(new long[]{1,2,2}, "")]
+        [InlineData(new long[]{1,2,3}, "Right")]
+        [InlineData(new long[]{1,3,2}, "Left")]
+        [InlineData(new long[]{1,-1,2}, "Right")]
+        [InlineData(new long[]{1,-1,-1}, "")]
+        [InlineData(new long[]{1,1}, "Left")]
+        [InlineData(new long[]{1,-1,1}, "Right")]
+        [InlineData(new long[]{1,1,1,1,2,1,3}, "Right")]
+        [InlineData(new long[]{1,1,1,2,2,1,3}, "")]
+        [InlineData(new long[]{1,1,1,2,2,1,3,1}, "Left")]
+        [InlineData(new long[]{1,1,1,2,2,2,3}, "Right")]
+        void Test(long[] arr, string expected)
+        {
+            LeftOrRightOfBinaryTreeSumLarger.Solution(arr).ShouldBe(expected);
+        }
+    }
+}
