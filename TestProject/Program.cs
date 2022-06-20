259e48ca1109f8756f4a7ca42720183aaa2c3daa
kbentley1 <46969522+kbentley1@users.noreply.github.com>
2022-06-20 12:05:01 +0100

    Add IComparable for F#

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace TestProject
+﻿using System;
+
+namespace TestProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var x = "C 13.01 (CR SEC) (CR SEC) Credit risk: Securitisations";
+            var result = x.Contains("(CR SEC) Credit Risk: Securitisations", StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
